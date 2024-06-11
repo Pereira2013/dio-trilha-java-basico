5b1dbb656a7e0d0f58d5fbda12eaaf186f9e9831
Robson Pereira <robson.pereira.ext@vr.com.br>
2024-06-11 15:39:03 -0300

    feat: adionado projeto com conhecimento de métodos

---
+++ tipos-variaveis/src/TipoVariavel.java
@@ -0,0 +1,13 @@
+public class TipoVariavel {
+    public static void main(String[] args) {
+    byte idade = 123;
+    short ano = 2021;
+    int cep = 21070333; // se começar com zero, talvez remova o zero da frente
+    long cpf = 849823749l;
+    float pi = 3.15F;
+
+
+
+        System.out.println("Hello world!");
+    }
+}
